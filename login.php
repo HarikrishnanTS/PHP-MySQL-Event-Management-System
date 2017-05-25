bb4e89c73b0a05705b7303b1107b6877333680a7
agbilotia1998 <agbilotia1998@gmail.com>
2017-05-25 15:20:29 +0530

    Added support for upload of only image files

--- login.php
+++ login.php
@@ -62,9 +62,89 @@ try {
                         <textarea class="form-control" id="message-text"></textarea>
                     </div>
 
+
                     <div class="form-group">
-                        <label for="recipient-name" class="control-label">Date of Event:</label>
-                        <input type="text" class="form-control" id="recipient-name">
+                        <label for="image" class="control-label">Upload Image for Event:</label>
+                        <input type="file" id="image" accept=".jpeg,.png,.gif,.jpg">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="doe" class="control-label">Date of Event:</label>
+                        <br>
+                            <in class="form-group">
+
+                                <label>DAY</label>
+                                <select>
+                                    <option>01</option>
+                                    <option>02</option>
+                                    <option>03</option>
+                                    <option>04</option>
+                                    <option>05</option>
+                                    <option>06</option>
+                                    <option>07</option>
+                                    <option>08</option>
+                                    <option>09</opt
+                                        ion>
+                                    <option>10</option>
+                                    <option>11</option>
+                                    <option>12</option>
+                                    <option>13</option>
+                                    <option>14</option>
+                                    <option>15</option>
+                                    <option>16</option>
+                                    <option>17</option>
+                                    <option>18</option>
+                                    <option>19</option>
+                                    <option>20</option>
+
+                                    <option>21</option>
+                                    <option>22</option>
+                                    <option>23</option>
+                                    <option>24</option>
+                                    <option>25</option>
+                                    <option>26</option>
+                                    <option>27</option>
+                                    <option>28</option>
+                                    <option>29</option>
+                                    <option>30</option>
+                                    <option>31</option>
+                                </select>
+
+                                <label>Month</label>
+                                <select>
+                                    <option>01</option>
+                                    <option>02</option>
+                                    <option>03</option>
+                                    <option>04</option>
+                                    <option>05</option>
+                                    <option>06</option>
+                                    <option>07</option>
+                                    <option>08</option>
+                                    <option>09</option>
+                                    <option>10</option>
+                                    <option>11</option>
+                                    <option>12</option>
+                                </select>
+
+                                <label>Year</label>
+                                <select>
+                                    <option>2017</option>
+                                    <option>2018</option>
+                                    <option>2019</option>
+                                    <option>2020</option>
+                                    <option>2021</option>
+                                    <option>2022</option>
+                                    <option>2023</option>
+                                    <option>2024</option>
+                                    <option>2025</option>
+                                    <option>2026</option>
+                                    <option>2027</option>
+                                    <option>2028</option>
+                                    <option>2029</option>
+                                    <option>2030</option>
+                                    <option>2031</option>
+                                    <option>2032</option>x
+                                </select>
                     </div>
                 </form>
             </div>
